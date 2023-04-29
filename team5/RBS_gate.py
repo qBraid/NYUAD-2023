94b92969b049e0908b3135e14e4a20dde4c81b74
Kannan Vijayadharan <kannan975@gmail.com>
2023-04-29 09:23:47 +0200

    RBS gate implemented

--- team5/RBS_gate.py
+++ team5/RBS_gate.py
@@ -1,26 +1,31 @@
+#%%
 from qiskit.circuit.gate import Gate
 from qiskit.circuit.quantumregister import QuantumRegister
 from qiskit.circuit.quantumcircuit import QuantumCircuit
+from qiskit.circuit.library.standard_gates import HGate, CZGate, RYGate
 
-# Create the RBS gate
-def RBSGate(param, qubit1, qubit2):
-    from qiskit.circuit.library.standard_gates.h import HGate, CZGate, RYGate
+param = 0.5
 
-    # Create a quantum circuit with two qubits
-    qr = QuantumRegister(2)
-    qc = QuantumCircuit(qr)
+# Create a quantum circuit with two qubits
+qr = QuantumRegister(2)
+qc = QuantumCircuit(qr)
 
-    # Apply hadamard on both qubits
-    qc.append(HGate(), [qubit1])
-    qc.append(HGate(), [qubit2])
-    # Apply a two qubit CZ gate
-    qc.append(CZGate(), [qubit1, qubit2])
-    # Apply RY of pi.2 on both qubits
-    qc.append(RYGate(param), [qubit1])
-    qc.append(RYGate(-param), [qubit2])
-    # Apply a two qubit CZ gate
-    qc.append(CZGate(), [qubit1, qubit2])
-    # Apply Hadamard on both qubits
-    qc.append(HGate(), [qubit1])
-    qc.append(HGate(), [qubit2])
+# Apply hadamard on both qubits
+qc.append(HGate(), [qr[0]])
+qc.append(HGate(), [qr[1]])
+# Apply a two qubit CZ gate
+qc.append(CZGate(), [qr[0], qr[1]])
+# Apply RY of pi.2 on both qubits
+qc.append(RYGate(param), [qr[0]])
+qc.append(RYGate(-param), [qr[1]])
+# Apply a two qubit CZ gate
+qc.append(CZGate(), [qr[0], qr[1]])
+# Apply Hadamard on both qubits
+qc.append(HGate(), [qr[0]])
+qc.append(HGate(), [qr[1]])
 
+# Draw the circuit
+qc.draw(output='mpl')
+
+
+# %%
