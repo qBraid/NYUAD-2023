568f370ddc99b9087e38a15265f6f7fbe5907e7a
Elias <elixaver@gmail.com>
2023-04-29 18:36:46 +0800

    boundary operator circuit appears correct

--- team5/quantum_circ_boundary.py
+++ team5/quantum_circ_boundary.py
@@ -24,7 +24,7 @@ from qiskit_aer import AerSimulator
 
 
 
-RUN_BACKEND = 0 # 0 = simulator
+RUN_BACKEND = 0 # 0 = simulator, 1 = Hardware, 2 = Unitary
 
 
 # convert normalized vector to spherical variables
@@ -58,27 +58,6 @@ def angles(x, V = True):
 
 
 
-# # RBS gate -- TODO: replace by custom gate
-# def apply_RBS(qc, qr, param):
-#     # Apply hadamard on both qubits
-#     qc.append(HGate(), [qr[0]])
-#     qc.append(HGate(), [qr[1]])
-#     # Apply a two qubit CZ gate
-#     qc.append(CZGate(), [qr[0], qr[1]])
-#     # Apply RY of pi.2 on both qubits
-#     qc.append(RYGate(param/2), [qr[0]])
-#     qc.append(RYGate(-param/2), [qr[1]])
-#     # Apply a two qubit CZ gate
-#     qc.append(CZGate(), [qr[0], qr[1]])
-#     # Apply Hadamard on both qubits
-#     qc.append(HGate(), [qr[0]])
-#     qc.append(HGate(), [qr[1]])
-    
-#     qc.draw(output = 'mpl')
-#     plt.show()
-#     return qc
-
-
 
 if __name__ == '__main__':
     n_q = 4
@@ -89,13 +68,16 @@ if __name__ == '__main__':
     qc = QuantumCircuit(qr)
 
 
-    thetas = angles(np.ones(n_q)/np.sqrt(n_q), V = False)*2
+    thetas = angles(np.ones(n_q)/np.sqrt(n_q), V = False)[::-1]*2 #factor of two to offset the multiplication by 1/2 in our convention of the RBS gate
+    #thetas = np.ones(n_q)*np.pi/4 * 2
     
     #prepares initial state: 1110 (corresponds to simple driangle)
-    #qc.x(0)
+    qc.x(0)
     #qc.x(1)
     #qc.x(2)
     
+    qc.barrier(range(n_q))
+
     
     #builds the circuit, fig. 2 from https://arxiv.org/pdf/2202.00054.pdf
     for ind, j in enumerate(range(2, n_q+1)):
@@ -107,6 +89,8 @@ if __name__ == '__main__':
     
         param = thetas[ind]
         
+        
+        # Apply RBS gate:
         qc.append(HGate(), [qr[i]])
         qc.append(HGate(), [qr[i+1]])
         # Apply a two qubit CZ gate
@@ -125,12 +109,13 @@ if __name__ == '__main__':
 
     for ind, j in enumerate(range(2, n_q+1)):
         i = j-2
+
         qr_pair = [qr[i], qr[i+1]]
         
         #TODO - remove
         #qc = apply_RBS(qc, qr_pair, thetas[i])
     
-        param = thetas[-ind]
+        param = thetas[::-1][ind]
         
         qc.append(HGate(), [qr[i]])
         qc.append(HGate(), [qr[i+1]])
@@ -147,27 +132,29 @@ if __name__ == '__main__':
 
 
     
-    # # Sample from the circuit
-    # meas = QuantumCircuit(n_q, n_q)
-    # meas.barrier(range(n_q))
-    # # map the quantum measurement to the classical bits
-    # meas.measure(range(n_q), range(n_q))
-    
-    # # append the measurement to our circuit
-    # qc = meas.compose(qc, range(n_q), front=True)
-    
-    
-        
-    # # Draw the circuit
-    # qc.draw(output='mpl')
+
     
     
     
     # Run the circuit
     if RUN_BACKEND == 0:
-        #backend = AerSimulator()
-        backend = Aer.get_backend('unitary_simulator') #to get unitary matrix
+        # Sample from the circuit
+        meas = QuantumCircuit(n_q, n_q)
+        meas.barrier(range(n_q))
+        # map the quantum measurement to the classical bits
+        meas.measure(range(n_q), range(n_q))
         
+        # append the measurement to our circuit
+        qc = meas.compose(qc, range(n_q), front=True)
+        
+        
+            
+        # Draw the circuit
+        qc.draw(output='mpl')
+        
+        
+        backend = AerSimulator()
+       
         # First we have to transpile the quantum circuit
         # to the low-level QASM instructions used by the
         # backend
@@ -184,18 +171,30 @@ if __name__ == '__main__':
         
         
         
-        print(np.real(result_sim.get_unitary(qc ,1).data))
-
         
+        counts = result_sim.get_counts(qc_compiled)
+        print(counts)
         
-        #counts = result_sim.get_counts(qc_compiled)
-        #print(counts)
-        
-        #from qiskit.visualization import plot_histogram
-        #plot_histogram(counts)
-        #plt.show()
+        from qiskit.visualization import plot_histogram
+        plot_histogram(counts)
+        plt.show()
     
     if RUN_BACKEND ==1: #run on real hardware
+        # Sample from the circuit
+        meas = QuantumCircuit(n_q, n_q)
+        meas.barrier(range(n_q))
+        # map the quantum measurement to the classical bits
+        meas.measure(range(n_q), range(n_q))
+        
+        # append the measurement to our circuit
+        qc = meas.compose(qc, range(n_q), front=True)
+        
+        
+            
+        # Draw the circuit
+        qc.draw(output='mpl')
+        
+        
         
         from qiskit_ibm_runtime import QiskitRuntimeService
         service = QiskitRuntimeService(channel="ibm_quantum")
@@ -214,3 +213,26 @@ if __name__ == '__main__':
             options=options,
             inputs=runtime_inputs,
         )
+        
+    if RUN_BACKEND == 2:
+        backend = Aer.get_backend('unitary_simulator')
+        
+               
+        # First we have to transpile the quantum circuit
+        # to the low-level QASM instructions used by the
+        # backend
+        qc_compiled = transpile(qc, backend)
+    
+        
+        # Execute the circuit on the qasm simulator.
+        # We've set the number of repeats of the circuit
+        # to be 1024, which is the default.
+        job_sim = backend.run(qc_compiled, shots=n_shots)
+        
+        result = job_sim.result()
+        print(np.real(result.get_unitary(qc ,3).data))
+
+        
+        
+        
+        
