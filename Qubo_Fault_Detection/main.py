87429c7cb3fe0280aca27cdd98697333fffb11bf
Basant-Elhussein <basantelhussein@aucegypt.edu>
2023-04-30 12:05:25 +0400

    update cost function

--- Qubo_Fault_Detection/main.py
+++ Qubo_Fault_Detection/main.py
@@ -1,47 +1,8 @@
 from pyqubo import Binary
 import neal
+import igraph
+from igraph import Graph, EdgeSeq
 
-
-
-# H = 0
-# sensor_readings = [0,0,1,1,0,0,1,1,0,0,1,1,1,1,1,1]
-
-#P = [[], [1, 2, 6], [1, 2, 7], [1, 2, 8], [1, 2, 9], [1, 3, 10], [1, 3, 11], [1, 3, 12], [1, 3, 13], [1, 4, 14], [1, 4, 15],
-#      [1, 4, 16], [1, 4, 17], [1, 5, 18], [1, 5, 19], [1, 5, 20], [1, 5, 21]]
-
-
-# for l in sensor_readings:
-#     for y in range(1, 17):
-#         if l == 1:
-#             H+= lambdaPath * Binary('y'+str(y)) * len(P[y])
-#             for x in P[y]:
-#                 H+= -lambdaPath * Binary('y'+str(y)) * Binary('x'+str(x))
-#         else:
-#             H+= lambdaPath*Binary('y'+str(y))*((1-len(P[y]))**2)
-#             for x in P[y]:
-#                 H+= lambdaPath*Binary('y'+str(y))*(Binary('x'+str(x))**2) + 2*lambdaPath*Binary('y'+str(y))*(1-len(P[y]))*Binary('x'+str(x))
-#                 for j in P[y]:
-#                     if x<j:
-#                         H+= 2*lambdaPath*Binary('y'+str(y))*Binary('x'+str(x))*Binary('x'+str(j))
-
-
-# for y in range(1, 17):
-#     H+= 1-Binary('y'+str(y))
-
-# for x in range(1, 22):
-#     H+= 1-Binary('x'+str(x))
-
-
-# model = H.compile()
-# bqm = model.to_bqm()
-
-# import neal
-# sa = neal.SimulatedAnnealingSampler()
-# sampleset = sa.sample(bqm, num_reads=10)
-# decoded_samples = model.decode_sampleset(sampleset)
-# best_sample = min(decoded_samples, key=lambda x: x.energy)
-
-# print(best_sample.sample)
 
 
 def read_input():
@@ -88,36 +49,6 @@ def create_optimization_function(sensor_readings, paths, number_of_nodes):
 
     return H
 
-
-def print_variables(best_sample):
-    Nodes = {}
-    Sensors = {}
-    for key in best_sample:
-        if "*" in key:
-            if best_sample[key] == 0:
-                pass
-            key1, key2 = [x for x in key.split(" * ")]
-            if "x" in key1:
-                Nodes[key1] = best_sample[key]
-            else:
-                Sensors[key1] = best_sample[key]
-            if "x" in key2:
-                Nodes[key2] = best_sample[key]
-            else:
-                Sensors[key2] = best_sample[key]
-        else:
-            if "x" in key:
-                Nodes[key] = best_sample[key]
-            else:
-                Sensors[key] = best_sample[key]
-    
-    print("Node Values:")
-    for key in Nodes:
-        print(key, ": ", Nodes[key])
-    
-    print("Sensor Values:")
-    for key in Sensors:
-        print(key, ": ", Sensors[key])
 
 
 if __name__ == '__main__':
